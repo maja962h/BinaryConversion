0ec7fd94408ecee793538f2188312b854317d25f
majamatboue <maja962h@stud.kea.dk>
2022-02-20 20:06:26 +0100

    Initial commit

---
+++ src/main/java/com/example/binaryconversion/BinaryConversionApplication.java
@@ -0,0 +1,13 @@
+package com.example.binaryconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryConversionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryConversionApplication.class, args);
+    }
+
+}
